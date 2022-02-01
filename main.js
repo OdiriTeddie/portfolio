8afb58b0cb5965858174e023beee168f99e68d5d
Odiri Teddie <odiriteddie.adamu@gmail.com>
2022-02-01 16:42:36 -0500

    more fix

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // import './style.css'
 
+const header = document.querySelector('.header')
+
+function headerChange() {
+    header.classList.toggle('sticky', window.scrollY > 0)
+}
+
+window.addEventListener('scroll', headerChange)
